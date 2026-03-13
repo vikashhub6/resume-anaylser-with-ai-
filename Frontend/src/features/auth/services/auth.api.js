6aebb1fe559fc270eb60bbac7823559835c11f16
Vikash Kumar <vkr122436@gmail.com>
2026-03-13 22:52:52 +0530

    Update API base URL to production environment

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -2,10 +2,9 @@ import axios from "axios"
 
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
-    withCredentials: true
+    baseURL: "https://resume-anaylser-with-ai.onrender.com",
+    withCredentials: true,
 })
-
 export async function register({ username, email, password }) {
 
     try {
