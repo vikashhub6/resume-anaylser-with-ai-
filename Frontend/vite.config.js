40d1074c21a881c428fd3a27fea592b3d97db0ad
Vikash Kumar <vkr122436@gmail.com>
2026-03-14 16:02:56 +0530

    Fix merge conflict in vite.config.js
    Resolve merge conflict in vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-=======
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
->>>>>>> d4f8b9ef35f6d0ab732b450a2a0e4c4f4b3dec04
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
-});
-=======
 })
->>>>>>> d4f8b9ef35f6d0ab732b450a2a0e4c4f4b3dec04
