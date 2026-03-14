5d735b1f3305fea7af58791b3fd2a9e262a3d170
vikashhub6 <vkr122436@gmail.com>
2026-03-14 16:47:21 +0530

    Fix: Resolve all merge conflict markers from local files

--- Backend/src/config/database.js
+++ Backend/src/config/database.js
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD:interview-ai-yt1/Backend/src/config/database.js
-const mongoose = require("mongoose")
-
-
-
-async function connectToDB() {
-
-    try {
-        await mongoose.connect(process.env.MONGO_URI)
-
-        console.log("Connected to Database")
-    }
-    catch (err) {
-        console.log(err)
-    }
-}
-
-module.exports = connectToDB
-=======
-=======
->>>>>>> d4f8b9ef35f6d0ab732b450a2a0e4c4f4b3dec04
 const mongoose = require("mongoose");
 
 async function connectToDB() {
@@ -33,7 +11,3 @@ async function connectToDB() {
 }
 
 module.exports = connectToDB;
-<<<<<<< HEAD
->>>>>>> 966552f (fixed pdf library for vercel):Backend/src/config/database.js
-=======
->>>>>>> d4f8b9ef35f6d0ab732b450a2a0e4c4f4b3dec04
--- Backend/src/controllers/interview.controller.js
+++ Backend/src/controllers/interview.controller.js
@@ -1,217 +1,107 @@
-<<<<<<< HEAD
-<<<<<<< HEAD:interview-ai-yt1/Backend/src/controllers/interview.controller.js
-const pdfParse = require("pdf-parse")
-=======
-const { extractText } = require("unpdf")
->>>>>>> d4f8b9ef35f6d0ab732b450a2a0e4c4f4b3dec04
-const { generateInterviewReport, generateResumePdf } = require("../services/ai.service")
-const interviewReportModel = require("../models/interviewReport.model")
-
-
-
+const pdfParse = require("pdf-parse-fork");
+const { generateInterviewReport, generateResumePdf } = require("../services/ai.service");
+const interviewReportModel = require("../models/interviewReport.model");
 
 /**
  * @description Controller to generate interview report based on user self description, resume and job description.
  */
 async function generateInterViewReportController(req, res) {
+  const resumeContent = await new pdfParse(Buffer.from(req.file.buffer)).getText();
+  const { selfDescription, jobDescription } = req.body;
 
-<<<<<<< HEAD
-    const resumeContent = await (new pdfParse.PDFParse(Uint8Array.from(req.file.buffer))).getText()
-=======
-    // Is line ko maine fork ke function-based syntax ke liye fix kiya hai
-  const { text } = await extractText(new Uint8Array(req.file.buffer))
-  const resumeContent = { text }
->>>>>>> d4f8b9ef35f6d0ab732b450a2a0e4c4f4b3dec04
-    const { selfDescription, jobDescription } = req.body
+  const interViewReportByAi = await generateInterviewReport({
+    resume: resumeContent.text,
+    selfDescription,
+    jobDescription,
+  });
 
-    const interViewReportByAi = await generateInterviewReport({
-        resume: resumeContent.text,
-        selfDescription,
-        jobDescription
-    })
+  const interviewReport = await interviewReportModel.create({
+    user: req.user.id,
+    resume: resumeContent.text,
+    selfDescription,
+    jobDescription,
+    ...interViewReportByAi,
+  });
 
-    const interviewReport = await interviewReportModel.create({
-        user: req.user.id,
-        resume: resumeContent.text,
-        selfDescription,
-        jobDescription,
-        ...interViewReportByAi
-    })
-
-    res.status(201).json({
-        message: "Interview report generated successfully.",
-        interviewReport
-    })
-
+  res.status(201).json({
+    message: "Interview report generated successfully.",
+    interviewReport,
+  });
 }
 
 /**
  * @description Controller to get interview report by interviewId.
  */
 async function getInterviewReportByIdController(req, res) {
+  const { interviewId } = req.params;
 
-    const { interviewId } = req.params
+  const interviewReport = await interviewReportModel.findOne({
+    _id: interviewId,
+    user: req.user.id,
+  });
 
-    const interviewReport = await interviewReportModel.findOne({ _id: interviewId, user: req.user.id })
+  if (!interviewReport) {
+    return res.status(404).json({
+      message: "Interview report not found.",
+    });
+  }
 
-    if (!interviewReport) {
-        return res.status(404).json({
-            message: "Interview report not found."
-        })
-    }
-
-    res.status(200).json({
-        message: "Interview report fetched successfully.",
-        interviewReport
-    })
+  res.status(200).json({
+    message: "Interview report fetched successfully.",
+    interviewReport,
+  });
 }
 
-
-<<<<<<< HEAD
-/** 
+/**
  * @description Controller to get all interview reports of logged in user.
-=======
-/** * @description Controller to get all interview reports of logged in user.
->>>>>>> d4f8b9ef35f6d0ab732b450a2a0e4c4f4b3dec04
  */
 async function getAllInterviewReportsController(req, res) {
-    const interviewReports = await interviewReportModel.find({ user: req.user.id }).sort({ createdAt: -1 }).select("-resume -selfDescription -jobDescription -__v -technicalQuestions -behavioralQuestions -skillGaps -preparationPlan")
+  const interviewReports = await interviewReportModel
+    .find({ user: req.user.id })
+    .sort({ createdAt: -1 })
+    .select(
+      "-resume -selfDescription -jobDescription -__v -technicalQuestions -behavioralQuestions -skillGaps -preparationPlan"
+    );
 
-    res.status(200).json({
-        message: "Interview reports fetched successfully.",
-        interviewReports
-    })
+  res.status(200).json({
+    message: "Interview reports fetched successfully.",
+    interviewReports,
+  });
 }
-
 
 /**
  * @description Controller to generate resume PDF based on user self description, resume and job description.
  */
 async function generateResumePdfController(req, res) {
-    const { interviewReportId } = req.params
+  const { interviewReportId } = req.params;
 
-    const interviewReport = await interviewReportModel.findById(interviewReportId)
+  const interviewReport = await interviewReportModel.findById(interviewReportId);
 
-    if (!interviewReport) {
-        return res.status(404).json({
-            message: "Interview report not found."
-        })
-    }
+  if (!interviewReport) {
+    return res.status(404).json({
+      message: "Interview report not found.",
+    });
+  }
 
-    const { resume, jobDescription, selfDescription } = interviewReport
+  const { resume, jobDescription, selfDescription } = interviewReport;
 
-    const pdfBuffer = await generateResumePdf({ resume, jobDescription, selfDescription })
+  const pdfBuffer = await generateResumePdf({
+    resume,
+    jobDescription,
+    selfDescription,
+  });
 
-    res.set({
-        "Content-Type": "application/pdf",
-        "Content-Disposition": `attachment; filename=resume_${interviewReportId}.pdf`
-    })
+  res.set({
+    "Content-Type": "application/pdf",
+    "Content-Disposition": `attachment; filename=resume_${interviewReportId}.pdf`,
+  });
 
-    res.send(pdfBuffer)
+  res.send(pdfBuffer);
 }
 
-<<<<<<< HEAD
-=======
-const pdfParse = require(""pdf-parse-fork")
-const { generateInterviewReport, generateResumePdf } = require("../services/ai.service")
-const interviewReportModel = require("../models/interviewReport.model")
-
-
-
-
-/**
- * @description Controller to generate interview report based on user self description, resume and job description.
- */
-async function generateInterViewReportController(req, res) {
-
-    const resumeContent = await (new pdfParse.PDFParse(Uint8Array.from(req.file.buffer))).getText()
-    const { selfDescription, jobDescription } = req.body
-
-    const interViewReportByAi = await generateInterviewReport({
-        resume: resumeContent.text,
-        selfDescription,
-        jobDescription
-    })
-
-    const interviewReport = await interviewReportModel.create({
-        user: req.user.id,
-        resume: resumeContent.text,
-        selfDescription,
-        jobDescription,
-        ...interViewReportByAi
-    })
-
-    res.status(201).json({
-        message: "Interview report generated successfully.",
-        interviewReport
-    })
-
-}
-
-/**
- * @description Controller to get interview report by interviewId.
- */
-async function getInterviewReportByIdController(req, res) {
-
-    const { interviewId } = req.params
-
-    const interviewReport = await interviewReportModel.findOne({ _id: interviewId, user: req.user.id })
-
-    if (!interviewReport) {
-        return res.status(404).json({
-            message: "Interview report not found."
-        })
-    }
-
-    res.status(200).json({
-        message: "Interview report fetched successfully.",
-        interviewReport
-    })
-}
-
-
-/** 
- * @description Controller to get all interview reports of logged in user.
- */
-async function getAllInterviewReportsController(req, res) {
-    const interviewReports = await interviewReportModel.find({ user: req.user.id }).sort({ createdAt: -1 }).select("-resume -selfDescription -jobDescription -__v -technicalQuestions -behavioralQuestions -skillGaps -preparationPlan")
-
-    res.status(200).json({
-        message: "Interview reports fetched successfully.",
-        interviewReports
-    })
-}
-
-
-/**
- * @description Controller to generate resume PDF based on user self description, resume and job description.
- */
-async function generateResumePdfController(req, res) {
-    const { interviewReportId } = req.params
-
-    const interviewReport = await interviewReportModel.findById(interviewReportId)
-
-    if (!interviewReport) {
-        return res.status(404).json({
-            message: "Interview report not found."
-        })
-    }
-
-    const { resume, jobDescription, selfDescription } = interviewReport
-
-    const pdfBuffer = await generateResumePdf({ resume, jobDescription, selfDescription })
-
-    res.set({
-        "Content-Type": "application/pdf",
-        "Content-Disposition": `attachment; filename=resume_${interviewReportId}.pdf`
-    })
-
-    res.send(pdfBuffer)
-}
-
->>>>>>> 966552f (fixed pdf library for vercel):Backend/src/controllers/interview.controller.js
-module.exports = { generateInterViewReportController, getInterviewReportByIdController, getAllInterviewReportsController, generateResumePdfController }
-=======
-module.exports = { generateInterViewReportController, getInterviewReportByIdController, getAllInterviewReportsController, generateResumePdfController }     
-
->>>>>>> d4f8b9ef35f6d0ab732b450a2a0e4c4f4b3dec04
+module.exports = {
+  generateInterViewReportController,
+  getInterviewReportByIdController,
+  getAllInterviewReportsController,
+  generateResumePdfController,
+};
