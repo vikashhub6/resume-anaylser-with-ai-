8f159b9c26fc7dde76ec25a8c40523077a7012be
vikashhub6 <vkr122436@gmail.com>
2026-03-14 16:37:27 +0530

    fix cors for vercel

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,33 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD:interview-ai-yt1/Backend/src/app.js
-const express = require("express")
-const cookieParser = require("cookie-parser")
-const cors = require("cors")
-
-const app = express()
-
-app.use(express.json())
-app.use(cookieParser())
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
-}))
-
-/* require all the routes here */
-const authRouter = require("./routes/auth.routes")
-const interviewRouter = require("./routes/interview.routes")
-
-
-/* using all the routes here */
-app.use("/api/auth", authRouter)
-app.use("/api/interview", interviewRouter)
-
-
-
-module.exports = app
-=======
-=======
->>>>>>> d4f8b9ef35f6d0ab732b450a2a0e4c4f4b3dec04
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
@@ -38,7 +8,10 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://resume-anaylser-with-ai.vercel.app"
+    ],
     credentials: true,
   }),
 );
@@ -52,7 +25,3 @@ app.use("/api/auth", authRouter);
 app.use("/api/interview", interviewRouter);
 
 module.exports = app;
-<<<<<<< HEAD
->>>>>>> 966552f (fixed pdf library for vercel):Backend/src/app.js
-=======
->>>>>>> d4f8b9ef35f6d0ab732b450a2a0e4c4f4b3dec04
