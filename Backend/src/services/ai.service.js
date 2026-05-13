567e692c2b2ba5d633dc0dad00c9a81bc81cd97e
Vikash Kumar <vkr122436@gmail.com>
2026-05-13 20:01:29 +0530

    model cahnge

--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -42,7 +42,7 @@ async function generateInterviewReport({ resume, selfDescription, jobDescription
 `
 
     const response = await ai.models.generateContent({
-        model: "gemini-3-flash-preview",
+        model: "gemini-2.0-flash",
         contents: prompt,
         config: {
             responseMimeType: "application/json",
@@ -96,7 +96,7 @@ async function generateResumePdf({ resume, selfDescription, jobDescription }) {
                     `
 
     const response = await ai.models.generateContent({
-        model: "gemini-3-flash-preview",
+        model: "gemini-2.0-flash",
         contents: prompt,
         config: {
             responseMimeType: "application/json",
