99ccf3e030e23454e9b23a8967187011698a7a7f
Vikash Kumar <vkr122436@gmail.com>
2026-05-13 23:23:55 +0530

    .env

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,17 +1,13 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
-
 const app = express();
-
 app.use(express.json());
 app.use(cookieParser());
-
-
 app.use(
   cors({
     origin: function(origin, callback) {
-      if (!origin || origin.endsWith('.vercel.app') || origin === 'http://localhost:5173') {
+      if (!origin || origin === process.env.FRONTEND_URL || origin === 'http://localhost:5173') {
         callback(null, true);
       } else {
         callback(new Error('Not allowed by CORS'));
@@ -20,13 +16,10 @@ app.use(
     credentials: true,
   }),
 );
-
 /* require all the routes here */
 const authRouter = require("./routes/auth.routes");
 const interviewRouter = require("./routes/interview.routes");
-
 /* using all the routes here */
 app.use("/api/auth", authRouter);
 app.use("/api/interview", interviewRouter);
-
 module.exports = app;
